867de72422b52dd7f4c0c3e81aabd9e47bc51ac4
erildojunior <erildojunior@live.com>
2020-06-01 09:48:25 -0300

    first version with all functions ok

--- book.js
+++ book.js
@@ -1,5 +1,12 @@
 let myLibrary = [];
 
+window.library = {
+  addBook,
+  removeBook,
+  editBook,
+  books: myLibrary,
+};
+
 function book(title, author, numberOfPages, ReadOrNot) {
   this.title = title;
   this.author = author;
@@ -18,64 +25,45 @@ function addBook(title, author, numberOfPages, ReadOrNot) {
 }
 
 function removeBook(name) {
+  let object = {};
   myLibrary.forEach((bookInfos) => {
     if (bookInfos.title == name) {
-      delete bookInfos;
-      console.log("book was removed with sucess!");
+      object = bookInfos;
     }
   });
+  myLibrary.splice(myLibrary.indexOf(object), 1);
+  myLibrary = myLibrary.filter((item) => item !== object);
 }
 
-function editBook(nameObject, id) {
-  switch (id) {
-    case "bookTitle":
-      nameObject.title = prompt("Write the new name of the Book");
+function editBook(categoryToEdit, titleOfTheBook, nameEdited) {
+  switch (categoryToEdit) {
+    case "title":
+      myLibrary.forEach((books) => {
+        if (books.title === titleOfTheBook) {
+          books.title = nameEdited;
+        }
+      });
       break;
     case "author":
-      nameObject.author = prompt("Write the new name of the Author");
+      myLibrary.forEach((books) => {
+        if (books.title === titleOfTheBook) {
+          books.author = nameEdited;
+        }
+      });
       break;
     case "numberOfPage":
-      nameObject.numberOfPages = prompt("Write the new number of pages");
+      myLibrary.forEach((books) => {
+        if (books.title === titleOfTheBook) {
+          books.numberOfPages = nameEdited;
+        }
+      });
       break;
     case "readOrNot":
-      nameObject.ReadOrNot = prompt("Write the correct answer");
+      myLibrary.forEach((books) => {
+        if (books.title === titleOfTheBook) {
+          books.ReadOrNot = nameEdited;
+        }
+      });
       break;
-  }
-}
-
-function menu() {
-  console.log("Welcome to your virtual library");
-  console.log("Write 1 to add a new book");
-  console.log("Write 2 to remove a book");
-  console.log("Write 3 to edit a book");
-  console.log("Write 4 to show your library");
-  console.log("Write 0 to close your menu");
-  let choose = prompt("your number");
-  if (choose === "1") {
-    let title = prompt("your title book");
-    let author = prompt("your author of book");
-    let numberOfPages = prompt("the number of pages");
-    let ReadOrNot = prompt("if you read or not the book");
-    addBook(title, author, numberOfPages, ReadOrNot);
-  }
-  if (choose === "2") {
-    let remove = prompt("Write te name of the book you want to remove");
-    myLibrary.splice(myLibrary.indexOf(object), 1);
-    array = array.filter((item) => item !== object);
-    removeBook(remove);
-  }
-  if (choose === "3") {
-    let bookSelected = "";
-    let title = prompt("Write the name of the book you want to edit");
-    myLibrary.forEach((books) => {
-      if (books.title === title) {
-        bookSelected = books;
-      }
-    });
-    let id = prompt("choose what you want to edit");
-    editBook(bookSelected, id);
-  }
-  if (choose === "4") {
-    console.log("It's your Library");
   }
 }
